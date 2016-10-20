43405084b5a38ab3a2597455c13de609de41b6d7
Brooke McKim <brookemckim@users.noreply.github.com>
2016-10-20 13:30:43 -0400

    Update parse.rb

--- parse.rb
+++ parse.rb
@@ -9,21 +9,30 @@ module Installer
     
     def configure!
       if role == 'db'
-        configure_database
+        install_database
+        setup_database
       elsif role == 'app'
-        configure_app
+        install_app
+        setup_app
+        run_app
       end
     end
     
-    def configure_database
+    def install_database
      system("sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && echo \"deb http://repo.mongodb.org/apt/ubuntu \"$(lsb_release -sc)\"/mongodb-org/3.0 multiverse\" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list")
      system("apt-get update && apt-get install -y mongodb-org")
     end
     
-    def configure_app
+    def setup_database
+      system("sed -i -e 's|bindIp: 127.0.0.1|bindIp: 0.0.0.0|g' /etc/mongod.conf")
+    end
+    
+    def install_app
       system("curl -sL https://deb.nodesource.com/setup_5.x | bash")
       system("apt-get install -y nodejs build-essential git")
-      
+    end
+    
+    def setup_app
       if application_repo
         system("git clone #{application_repo}")
       end
