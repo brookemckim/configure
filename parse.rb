4c9f58fbb83f57dfbadcabb0a984ef2ece78921c
Brooke McKim <brookemckim@users.noreply.github.com>
2016-10-20 12:56:07 -0400

    Create parse.rb

---
+++ parse.rb
@@ -0,0 +1,32 @@
+module Installer
+  class Parse
+    def new(role, application_repo)
+      @role = role
+      @application_repo = application_repo
+    end
+    
+    attr_reader :role, :application_repo
+    
+    def configure!
+      if role == 'database'
+        configure_database
+      elsif role == 'app'
+        configure_app
+      end
+    end
+    
+    def configure_database
+     system("sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list")
+     system("apt-get update && apt-get install -y mongodb-org")
+    end
+    
+    def configure_app
+      system("curl -sL https://deb.nodesource.com/setup_5.x | bash")
+      system("apt-get install -y nodejs build-essential git")
+      
+      if application_repo
+        system("git clone #{application_repo}")
+      end
+    end
+  end
+end
